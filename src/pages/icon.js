c91672371f8a00b7e717402a3364e0968219ce38
ValeriaVG <valeria.viana.gusmao@gmail.com>
2019-05-13 18:52:32 +0200

    Less worse way of adding icons

--- src/pages/icon.js
+++ src/pages/icon.js
@@ -1,23 +1,36 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fas } from '@fortawesome/free-solid-svg-icons'
 
-library.add(fas)
-
-const Icon = ({ match }) => (
-  <>
-    <Helmet>
-      <title> Icon {match.params.name}</title>
-      <meta name="description" content={`Icon ${match.params.name}`} />
-    </Helmet>
-    <div className="App">
-      <header className="App-header">
-        <FontAwesomeIcon icon={match.params.name} prefix="fas" size="10x" />
-        <h1>Icon {match.params.name}</h1>
-      </header>
-    </div>
-  </>
-)
-export default Icon
+export default class Icon extends React.PureComponent {
+  state = {
+    isLoaded: false,
+  }
+  async componentDidMount() {
+    const { library } = await import('@fortawesome/fontawesome-svg-core')
+    const { FontAwesomeIcon } = await import('@fortawesome/react-fontawesome')
+    const { fas } = await import('@fortawesome/free-solid-svg-icons')
+    library.add(fas)
+    this.library = library
+    this.FontAwesomeIcon = FontAwesomeIcon
+    this.setState({ isLoaded: true })
+  }
+  render() {
+    const name = this.props.match.params.name
+    return (
+      <>
+        <Helmet>
+          <title> Icon {name}</title>
+          <meta name="description" content={`Icon ${name}`} />
+        </Helmet>
+        <div className="App">
+          <header className="App-header">
+            {this.state.isLoaded && (
+              <this.FontAwesomeIcon icon={name} prefix="fas" size="10x" />
+            )}
+            <h1>Icon {name}</h1>
+          </header>
+        </div>
+      </>
+    )
+  }
+}
